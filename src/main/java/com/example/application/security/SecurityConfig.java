a3ab354ecc7f7a7750808f1a189de5424aa202f8
wreckingralph06 <ralphwaldocandaza@gmail.com>
2024-06-09 07:21:34 +0800

    Added Security configuration... 1. Setup default login view 2. Ignore images folder 3. Added default user creds to login (user and password)

---
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.application.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import com.example.application.views.LoginView;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends VaadinWebSecurity {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		setLoginView(http, LoginView.class);
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().requestMatchers("/images/**");
+		super.configure(web);
+	}
+	
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(User.withUsername("user")
+				.password("{noop}userpass")
+				.roles("USER")
+				.build());
+	}
+}
